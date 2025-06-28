ad4d081de04b808a9bea3d9dd3fab4954a8e40fa
Dennis <macdehnis301@gmail.com>
2025-06-28 19:58:05 +0100

    Jenkins python build

---
+++ index.py
@@ -0,0 +1,7 @@
+car = "Toyota"
+
+car_brands = ["Honda", "Ford", "BMW", "Toyota", "Nissan", "Mazda", "Chevrolet"]
+
+for brand in car_brands:
+    if brand == car:
+        print("Yes, it is available")
